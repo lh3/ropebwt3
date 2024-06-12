87a7fb48514ca9f729371b4b91d98d8be361c3be
Heng Li <lh3@me.com>
2024-06-11 21:19:23 -0400

    Release ropebwt3-3.0 (r67)

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "io.h"
 #include "ketopt.h"
 
-#define RB3_VERSION "3.0pre-r65"
+#define RB3_VERSION "3.0-r67"
 
 int main_build(int argc, char *argv[]);
 int main_merge(int argc, char *argv[]);
